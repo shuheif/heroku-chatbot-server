5cd31cd4aaa74be13d2da9bef9a74263ee0e2ca7
shuheif <shuheifujita95@gmail.com>
2018-08-23 10:49:22 -0400

    here

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
+	res.send('here')
 	request({
 			    url: 'https://friends-chatbot.herokuapp.com/prediction',
 			    method: 'POST',
